b498edc08c0af30c5fe1ebad4b9115c6730d0e7b
Corbin Taylor <cptaylor38@yahoo.com>
2020-08-15 23:04:04 -0400

    fixed

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -8,36 +8,19 @@ const authReducer = (state, action) => {
   }
 };
 
-const signup = async (dispatch) => {
+const signup = (dispatch) => {
   return async ({ email, password }) => {
     try {
       const response = await trackerAPI.post('/signup', { email, password });
       console.log(response.data);
     } catch (err) {
-      console.log(error.msg);
+      console.log(err);
     }
   };
 };
 
-const signin = (dispatch) => {
-  return ({ email, password }) => {
-    try {
-      //make api request to sign in with {email, password}
-      //sign in success = modify state and change to authenticated for main app
-    } catch (err) {
-      //more error handling
-    }
-  };
-};
-
-const signout = (dispatch) => {
-  return {
-    // make api request to sign out
-  };
-};
-
-export const { Provder, Context } = createDataContext(
+export const { Provider, Context } = createDataContext(
   authReducer,
-  {},
-  { isSignedIn: false }
+  { signup },
+  { isSignedIn: true }
 );
